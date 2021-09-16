b7d926a63ef67975051098f766050480323ea019
EmptyBP <90006396+EmptyBP@users.noreply.github.com>
2021-09-16 16:59:53 -0500

    Update Pig_Latin.java

--- Pig_Latin.java
+++ Pig_Latin.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 
 		 String add1="way";
 		 String add2= "ay";
-		 
-
+		 int lenght1;
+		
 		char[] vowels= {'a','e','i','o','u'}; //Para vocales / Save vowels
 		
 		String respuesta;  
 		
-		char primL, secL; 
+		char primL, secL, terL;
 		/*Estos char se usarán para la función lógica del Pig Latin entre consonanates y vocales 
 		 * Those char will be use as logic conditions for Pig latin between consonants and vowels*/
 		
-    }
+		//Ciclo for para guardar las consonantes del alfabeto / loop for to save alphabet consonants
         //Aquí empieza la interacción con el usuario / At this point Starts the interaction with the user
         
         System.out.println("¡Bienvenido al traductor Pig Latín, ingresa el texto que desees (en inglés) y será traducido"
@@ -28,45 +28,70 @@ import java.util.Scanner;
         respuesta=in.nextLine();
         respuesta=respuesta.toLowerCase();
         
-        String[] words =respuesta.split("\\s+");
-        String espacio="";
+        String[] words =respuesta.split("\\W+");
         
         for (String word : words) {
         	
         primL=word.charAt(0);
-        
-        
+        lenght1=word.length();
+        //consonantes
         if(primL != vowels[0] && primL != vowels[1] && primL !=vowels[2]  && primL !=vowels[3]  && primL !=vowels[4]) {
         	secL=word.charAt(1);
-        	if (secL != vowels[0] && secL != vowels[1] && secL !=vowels[2]  && secL !=vowels[3]  && secL !=vowels[4]) {
+        	
+        	
+        	//Excepcion
+        	if (secL != vowels[0] && secL != vowels[1] && secL !=vowels[2]  && secL !=vowels[3] && lenght1>=3) {
+        		terL=word.charAt(2);
+        		if (lenght1<3) {
+            		String exception="";
+            		for (int i=1; i<word.length();i++) {
+            			exception=exception + word.charAt(i);
+            		}
+            		
+            		exception=exception+word.charAt(0)+add2;
+                		System.out.print(exception+" ");
+                		}
+        		
+        		//3 consonantes
+        		else if (terL != vowels[0] && terL != vowels[1] && terL !=vowels[2]  && terL !=vowels[3]  && terL !=vowels[4]) {
+            		String word2="";
+            		for (int i=3; i<word.length();i++) {
+            			word2=word2 + word.charAt(i);
+            		}
+            		
+            		word2=word2+word.charAt(0)+word.charAt(1)+word.charAt(2)+add2;
+            		System.out.print(word2+" ");
+            		
+        		}
+        		
+        		//2 consonantes
+        		else if (terL == vowels[0] || terL == vowels[1] || terL == vowels[2]  || terL == vowels[3]  || terL == vowels[4]) {
         		String word1="";
         		for (int i=2; i<word.length();i++) {
         			word1=word1 + word.charAt(i);
         		}
-        		word1=word1+word.charAt(0)+word.charAt(1)+add2;
-        		System.out.print(word1+" ");
-        	
+        			word1=word1+word.charAt(0)+word.charAt(1)+add2;
+            		System.out.print(word1+" ");
         	}
         		
+        		}
+        	
+        		//1 consonante
         		else {
-        				String word2="";
+        				String word3="";
         				for (int i=1; i<word.length();i++) {
-            			word2=word2 + word.charAt(i);
+            			word3=word3 + word.charAt(i);
         				}
-        				word2=word2+word.charAt(0)+add2;
-        				System.out.print(word2+" ");
+        				word3=word3+word.charAt(0)+add2;
+        				System.out.print(word3+" ");
         				
         				
         		}
         } 
+        //Vocal
         if (primL == vowels[0] || primL == vowels[1] || primL ==vowels[2]  || primL ==vowels[3]  || primL ==vowels[4]){
         	
-        		String word3="";
-        		for (int i=1; i<word.length();i++) {
-        			word3=word3 + word.charAt(i);
-        		}
-        		word3=word3+word.charAt(0)+add1;
-        		System.out.print(word3+" ");
+        		System.out.print(word.concat(add1)+" ");
         	
         		}
         
@@ -74,5 +99,3 @@ import java.util.Scanner;
         }
 	}
  
-  
-
